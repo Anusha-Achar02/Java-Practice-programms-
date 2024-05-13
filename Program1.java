a2939b3b4e9e84e5e9c32c3f6859065cc11643b9
Anusha-Achar02 <164835249+Anusha-Achar02@users.noreply.github.com>
2024-05-13 13:24:03 +0530

    Add files via upload
    Java program to create an iteger array by taking its length as user input store the numbers in the array and display the content of array

--- Program1.java
+++ Program1.java
@@ -1,33 +1,21 @@
 import java.util.Scanner;
 class Program1 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		Scanner scan = new Scanner (System.in);
-		System.out.println("Enter the number of schools,classes and students");
-		int[][][]arr = new int[scan.nextInt()][scan.nextInt()][scan.nextInt()];
-		for (int i=0;i<=arr.length-1;i++)
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		int[]arr = new int[scan.nextInt()];
+		System.out.println("Enter the elements in array");
+		for(int i=0;i<=arr.length-1;i++)
 		{
-			for (int j=0;j<=arr.length-1;j++)
+			arr[i]=scan.nextInt();
+		}
+			System.out.println("The elements stored are:");
+			for(int i=0;i<=arr.length-1;i++)
 			{
-				for (int k=0;k<=arr.length-1;k++)
-				{
-					System.out.println("Enter the marks of school:"+(i+1)+"class:"+(j+1)+"students"+(k+1));
-					arr[i][j][k]=scan.nextInt();
-				}
+				System.out.println(arr[i]);
 			}
-		}
-		for (int i=0;i<=arr.length-1;i++)
-		{
-			for (int j=0;j<=arr.length-1;j++)
-			{
-				for (int k=0;k<=arr.length-1;k++)
-				{
-					System.out.println(arr[i][j][k]+" ");
-				}
-				System.out.println();
-	            }
-			    System.out.println();
-		}
 	}
+
 }
